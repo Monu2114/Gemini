3a94678f097e48dd32e31c79f507d414bb5b07bc
Monu_2114 <monu.nanabala@gmail.com>
2024-04-29 10:13:22 +0530

    Original and Corrected code

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const app = express();
-
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI('AIzaSyABUmpgsYwDGsqeqsw0vG8UXDj5RCWyq3Y');
+const cors = require('cors');
+app.use(cors());
 
   app.use(express.json());
+
+  async function generateResponse(prompt) {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    return text;
+}
 
   app.post('/takeInput', async (req, res) => {
       try {
           const prompt = req.body.prompt;
           const response = await generateResponse(prompt);
-          res.json({response});
+          res.json(response);
       } catch (error) {
           console.log('Error:', error);
       }
   });
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
